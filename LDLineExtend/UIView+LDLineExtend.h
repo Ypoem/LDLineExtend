eb6a2a94f14a5de9a1956508cdb12dceddae8c5c
lidi <1078805781@qq.com>
2015-10-20 19:28:42 +0800

    Initial Commit

---
+++ LDLineExtend/UIView+LDLineExtend.h
@@ -0,0 +1,13 @@
+//
+//  UIView+LDLineExtend.h
+//  LDLineExtend
+//
+//  Created by lidi on 10/20/15.
+//  Copyright © 2015 lidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (LDLineExtend)
+
+@end
---
+++ LDLineExtend/UIView+LDLineExtend.m
@@ -0,0 +1,13 @@
+//
+//  UIView+LDLineExtend.m
+//  LDLineExtend
+//
+//  Created by lidi on 10/20/15.
+//  Copyright © 2015 lidi. All rights reserved.
+//
+
+#import "UIView+LDLineExtend.h"
+
+@implementation UIView (LDLineExtend)
+
+@end
